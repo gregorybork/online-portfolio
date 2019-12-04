83200eda6b858e94fc97d805d45d287ca39b2dde
Greg <gregorybork1@gmail.com>
2019-12-04 04:35:38 -0800

    preloader

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
 
+  get 'preloader/logo'
+  get 'rails/g'
+  get 'rails/controller'
+  get 'rails/preloader'
+  get 'rails/logo'
   resources :quotes
   root 'home#index'
   resources :home, only: [:index, :new, :create]
